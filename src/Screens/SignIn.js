70bda10e96ab6a6e30813a005430e20cb00138bb
Dumitras Daniel <dumitras.daniell@gmail.com>
2023-10-07 17:03:03 +0200

    Resolved authentification with firebase

--- src/Screens/SignIn.js
+++ src/Screens/SignIn.js
@@ -3,28 +3,39 @@ import { View,StyleSheet, Image } from 'react-native';
 import InputBox from '../Components/InputBox';
 import CustomButton from '../Components/CustomButton';
 import Logo from '../assets/logo.png'
+import { auth } from '../../firebase'
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 const SignInScreen = () => {
-    const [username,setUsername] = useState('');
+    const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
+
+    const onSignInPressed = () => {
+        signInWithEmailAndPassword(auth,email,password)
+        .then((re)=>{
+            console.log("OK")
+        })
+        .catch(error => alert(error.message))
+    };
+    
+    const onSignUpPressed = () => {
+        createUserWithEmailAndPassword(auth,email,password)
+        .then((userCredentials) => {
+            console.log(userCredentials);
+        })
+        .catch(error => alert(error.message))
+    };
 
     return (
         <View style={styles.container}>
             <Image source={Logo} style={styles.logoStyle}/>
-            <InputBox placeholder="Username" value={username} setValue={setUsername}/>
+            <InputBox placeholder="Email" value={email} setValue={setEmail}/>
             <InputBox placeholder="Password" value={password} setValue={setPassword}/>
             <CustomButton action='Sign in' onPress={onSignInPressed} backcolor={'#26be81'} bordercolor={'#26be81'} textcolor={'white'}/>
             <CustomButton action='Sign up' onPress={onSignUpPressed} backcolor={'white'} bordercolor={'#26be81'} textcolor={'#26be81'}/>
         </View>
     )
-};
-
-const onSignInPressed = () => {
-    console.warn('Signed in');
-};
-
-const onSignUpPressed = () => {
-    console.warn('Signed up')
 };
 
 const styles = StyleSheet.create({
