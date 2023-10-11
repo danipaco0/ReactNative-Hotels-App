9fa483f51cc51a2bd83b69da24fd75a2d6174329
Dumitras Daniel <dumitras.daniell@gmail.com>
2023-10-11 17:22:42 +0200

    Configured change of position of the search box when the map is dragged

--- src/Screens/HomePage.js
+++ src/Screens/HomePage.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import InputBox from '../Components/InputBox';
 import MapView, { PROVIDER_GOOGLE, Marker} from 'react-native-maps';
-import { View,StyleSheet, Button } from 'react-native';
+import { View,StyleSheet, Button, TouchableHighlight } from 'react-native';
 import CustomButton from '../Components/CustomButton';
 
 export default function App() {
@@ -10,19 +10,29 @@ export default function App() {
         longitude: 4.378989,
         latitudeDelta: 0.009,
         longitudeDelta: 0.009
-    }
-    
+    };
+
     const [city,setCity] = useState('');
+    const [searchBottom, setSearchBottom] = useState(40);
+    const [mapFocus, setMapFocus] = useState(false);
+    
+    const onMapDrag = () => {
+        if(!mapFocus){
+            setSearchBottom(searchBottom-80);
+            setMapFocus(true);
+        }
+    };
 
     return (
         <View style={styles.container}>
-            <MapView style={styles.map}
-            provider={PROVIDER_GOOGLE}
-            mapType='standard'
-            region={location}>
-                <Marker coordinate={{ latitude: 50.811662, longitude: 4.378989 }}/>
+            <MapView style={styles.container}
+                provider={PROVIDER_GOOGLE}
+                mapType='standard'
+                initialRegion={location}
+                onRegionChangeComplete={onMapDrag}>
+                    <Marker coordinate={{ latitude: 50.811662, longitude: 4.378989 }}/>
             </MapView>
-            <View style={styles.searchBox}>
+            <View style={[styles.searchBox,{bottom:searchBottom}]}>
                 <InputBox placeholder="City" value={city} setValue={setCity} bordercolor={'#26be81'}/>
                 <CustomButton action="Search" onPress="Nothing" backcolor={'#26be81'} bordercolor={'#26be81'} textcolor={'white'}/>
             </View>
@@ -34,9 +44,6 @@ const styles = StyleSheet.create({
     container:{
         flex:1
     },
-    map:{
-        flex:1
-    },
     searchBox:{
         position:'absolute',
         alignSelf:'center',
@@ -45,7 +52,6 @@ const styles = StyleSheet.create({
         backgroundColor:'white',
         width:'90%',
         height:150,
-        bottom:30,
         borderRadius:10,
         borderWidth:2,
         borderColor:'#26BE81'
